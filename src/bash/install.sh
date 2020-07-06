5e635fcc8f692050a535838bfca8314386b7e1ea
Jesse Cox <jess.coxpdx@gmail.com>
2020-07-06 14:04:51 -0700

    Add files via upload

---
+++ src/bash/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Create destination folder
+DESTINATION="/etc/rt_updater"
+VERSION="0.0.1"
+NAME="rt_updater_v${VERSION}.tgz"
+URL="https://github.com/jessejamescox/eruntime-project-updater/raw/master/${NAME}"
+mkdir -p ${DESTINATION}
+
+wget ${URL}
+tar -xzf ${NAME} -C /etc
+
+echo ""
+echo "Installation complete."
+echo ""
+
+rm ${NAME}
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "5 * * * * python /etc/rt_updater/rt_updater_v0.0.1.py >/dev/null 2>&1" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
+
+# Exit from the script with success (0)
+exit 0
